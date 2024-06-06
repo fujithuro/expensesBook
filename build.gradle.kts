1f61f819b1e297121fde798bc64613e7fa97dce1
fujita <fujithuro@gmail.com>
2024-06-06 23:40:27 +0900

    feat: DBアクセスに必要な設定や依存関係を追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
