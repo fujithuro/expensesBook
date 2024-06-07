e0922f481ea9615aed6b1ccf5cef4c00b6184553
fujita <fujithuro@gmail.com>
2024-06-07 11:58:24 +0900

    feat: 出費情報のINSERTを実装。新規登録されたidを呼び出し元に返す。

--- src/main/kotlin/com/thurofuji/expensesBook/repository/ExpenseBookRepository.kt
+++ src/main/kotlin/com/thurofuji/expensesBook/repository/ExpenseBookRepository.kt
@@ -3,6 +3,7 @@ package com.thurofuji.expensesBook.repository
 import com.thurofuji.expensesBook.model.ExpenseRequest
 import org.springframework.stereotype.Repository
 import org.springframework.jdbc.core.simple.JdbcClient
+import java.util.UUID
 
 /**
  * 家計簿に関する情報の永続化、および永続化された情報へのアクセスを行うRepositoryクラス
@@ -12,10 +13,21 @@ class ExpenseBookRepository(private val jdbcClient: JdbcClient) {
     // TODO JdbcClientを通してDBアクセス
 
     /**
-     * 出費情報（[expense]）を永続化する
+     * 出費情報（[expense]）を永続化し、登録されたidを[UUID]で返す
      */
-    fun register(expense: ExpenseRequest) {
-        // TODO 出費情報のINSERT
+    fun register(expense: ExpenseRequest): UUID {
+        return jdbcClient.sql("""
+            INSERT INTO 出費 (購入日, 金額, 店舗, 費目cd)
+            VALUES (?, ?, ?, ?)
+            RETURNING id
+        """.trimIndent())
+            .params(
+                expense.date
+                , expense.price
+                , expense.store
+                , expense.type
+            ).query(UUID::class.java)
+            .single()
     }
 
 }
