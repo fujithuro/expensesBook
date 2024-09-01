cc5204374c108b746d7ff57d95ea23e0ffa89d5c
fujita <fujithuro@gmail.com>
2024-09-01 14:12:37 +0900

    chore: 入力値検証に関する機能を扱うため、`spring-boot-starter-validation`を依存関係に追加
    refs #5 サーバー側入力値検証の追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-web")
