6fbcd7f3f1fce23983eb168b36fb572ebe425eb0
fujita <fujithuro@gmail.com>
2024-09-05 22:53:58 +0900

    feat: 費目を扱うモデルで、使用する型を列挙型ではなく`ExpenseTypeDto`に変更する。まだモデルを扱うコードは変更していないので、このコミット時点ではコンパイルエラーとなる。
    refs #11 費目の扱いを整理する

--- src/main/kotlin/com/thurofuji/expensesBook/dto/ListSearchCondition.kt
+++ src/main/kotlin/com/thurofuji/expensesBook/dto/ListSearchCondition.kt
@@ -6,4 +6,4 @@ import java.time.YearMonth
  * 出費の一覧を検索するための条件を表すモデル
  */
 data class ListSearchCondition(val targetYearMonth: YearMonth
-                               , val typeList: List<ExpenseType>)
+                               , val typeList: List<ExpenseTypeDto>)
