db615cd4df0493ec4ea5cbf92420270f8de26ec7
fujita <fujithuro@gmail.com>
2024-09-08 00:16:30 +0900

    feat: リストに格納されている場合でも、アノテーションで費目がチェックできるようにValidatorを追加。まだ参照はされていない。

--- src/main/kotlin/com/thurofuji/expensesBook/annotation/ValidExpenseType.kt
+++ src/main/kotlin/com/thurofuji/expensesBook/annotation/ValidExpenseType.kt
@@ -1,13 +1,14 @@
 package com.thurofuji.expensesBook.annotation
 
 import com.nimbusds.jose.Payload
+import com.thurofuji.expensesBook.validation.ExpenseTypeListValidator
 import com.thurofuji.expensesBook.validation.ExpenseTypeValidator
 import jakarta.validation.Constraint
 import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
-@Constraint(validatedBy = [ExpenseTypeValidator::class])
+@Constraint(validatedBy = [ExpenseTypeValidator::class, ExpenseTypeListValidator::class])
 annotation class ValidExpenseType(
     val message: String = "Invalid expense type.",
     val groups: Array<KClass<*>> = [],
---
+++ src/main/kotlin/com/thurofuji/expensesBook/validation/ExpenseTypeListValidator.kt
@@ -0,0 +1,26 @@
+package com.thurofuji.expensesBook.validation
+
+import com.thurofuji.expensesBook.annotation.ValidExpenseType
+import com.thurofuji.expensesBook.exception.InvalidExpenseTypeException
+import com.thurofuji.expensesBook.service.ExpenseTypeService
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+
+class ExpenseTypeListValidator(private val expenseTypeService: ExpenseTypeService):
+    ConstraintValidator<ValidExpenseType, List<Int>> {
+
+    override fun isValid(value: List<Int>?, context: ConstraintValidatorContext): Boolean {
+        if (value == null) {
+            return true // nullチェックは`@NotNull`で行われるので、ここでは対象外
+        }
+        return value.all { type ->
+            try {
+                expenseTypeService.getExpenseType(type)
+                true
+            } catch (e: InvalidExpenseTypeException) {
+                false
+            }
+        }
+    }
+
+}
