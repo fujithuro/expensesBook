eeb0af8ba9f47d57f62a5298f17f3f7256f80bb0
fujita <fujithuro@gmail.com>
2024-09-07 16:30:24 +0900

    feat: 発生した問題についてより詳細な情報を伝えられるよう、独自例外を定義。まだ使っていない。

---
+++ src/main/kotlin/com/thurofuji/expensesBook/exception/ExpenseBookException.kt
@@ -0,0 +1,9 @@
+package com.thurofuji.expensesBook.exception
+
+/**
+ * 家計簿アプリケーションで発生した問題を扱うための独自例外
+ */
+sealed class ExpenseBookException(message: String): RuntimeException(message)
+
+/** 費目コードが不正な値だった場合の例外 */
+class InvalidExpenseTypeException(message: String): ExpenseBookException(message)
