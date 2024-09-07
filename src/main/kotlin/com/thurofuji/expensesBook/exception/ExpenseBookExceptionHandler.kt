557e5194f4ae253a3c45484d304be15dcd9ec405
fujita <fujithuro@gmail.com>
2024-09-08 00:42:14 +0900

    chore: 不要になったimportの削除漏れ

--- src/main/kotlin/com/thurofuji/expensesBook/exception/ExpenseBookExceptionHandler.kt
+++ src/main/kotlin/com/thurofuji/expensesBook/exception/ExpenseBookExceptionHandler.kt
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.method.annotation.HandlerMethodValidationException
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
-import java.time.format.DateTimeParseException
 
 @RestControllerAdvice
 class ExpenseBookExceptionHandler {
