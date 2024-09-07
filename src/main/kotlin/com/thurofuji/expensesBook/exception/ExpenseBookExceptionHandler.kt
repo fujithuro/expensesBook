669cb8a48fb03f0cd5b75666fcca6448618cb38c
fujita <fujithuro@gmail.com>
2024-09-07 17:17:46 +0900

    feat: グローバルな例外ハンドラを作成したので、1つの例外ハンドラで複数の例外を扱うのではなく、個々の例外に応じた対応を行えるように関数を分ける。まだ、分けただけでやっている処理は同じのまま。

--- src/main/kotlin/com/thurofuji/expensesBook/exception/ExpenseBookExceptionHandler.kt
+++ src/main/kotlin/com/thurofuji/expensesBook/exception/ExpenseBookExceptionHandler.kt
@@ -15,25 +15,53 @@ class ExpenseBookExceptionHandler {
 
     private val logger: Logger = LoggerFactory.getLogger(ExpenseBookExceptionHandler::class.java)
 
+
     /**
-     * 入力値検証で例外がスローされた場合のハンドリングを行う。
-     * この例外ハンドラでは主に、「入力値の型やフォーマットが一致せず、引数に値を設定できなかった」など引数の問題で例外がスローされた場合のハンドリングを担当している。
-     *
-     * [MethodArgumentTypeMismatchException]: 引数に対して型が不一致な場合にスローされる
-     * [MethodArgumentNotValidException]: メソッドの引数に対する入力値検証で問題が見つかった場合にスローされる
-     * [HttpMessageNotReadableException]: リクエスト内容をオブジェクトに展開できなかった場合にスローされる。たとえばnull非許容のプロパティにnullが送信された、型が合致しないなど
-     * [InvalidExpenseTypeException]: リクエストされた費目に問題があった場合にスローされる
-     * [DateTimeParseException]: 年月日のパースに失敗した場合にスローされる
+     * [MethodArgumentTypeMismatchException]のハンドリングを行う
      */
     @Suppress("UNUSED")
-    @ExceptionHandler(
-        MethodArgumentTypeMismatchException::class
-        , MethodArgumentNotValidException::class
-        , HttpMessageNotReadableException::class
-        , InvalidExpenseTypeException::class
-        , DateTimeParseException::class
-    )
-    fun handleValidationException(ex: Exception): ResponseEntity<Void> {
+    @ExceptionHandler(MethodArgumentTypeMismatchException::class)
+    fun handleMethodArgumentTypeMismatchException(ex: MethodArgumentTypeMismatchException): ResponseEntity<Void> {
+        logger.info("Input validation failed.: {}", ex.message)
+        return ResponseEntity.badRequest().build()
+    }
+
+    /**
+     * [MethodArgumentNotValidException]のハンドリングを行う
+     */
+    @Suppress("UNUSED")
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException): ResponseEntity<Void> {
+        logger.info("Input validation failed.: {}", ex.message)
+        return ResponseEntity.badRequest().build()
+    }
+
+    /**
+     * [HttpMessageNotReadableException]のハンドリングを行う
+     */
+    @Suppress("UNUSED")
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(ex: HttpMessageNotReadableException): ResponseEntity<Void> {
+        logger.info("Input validation failed.: {}", ex.message)
+        return ResponseEntity.badRequest().build()
+    }
+
+    /**
+     * [InvalidExpenseTypeException]のハンドリングを行う
+     */
+    @Suppress("UNUSED")
+    @ExceptionHandler(InvalidExpenseTypeException::class)
+    fun handleInvalidExpenseTypeException(ex: InvalidExpenseTypeException): ResponseEntity<Void> {
+        logger.info("Input validation failed.: {}", ex.message)
+        return ResponseEntity.badRequest().build()
+    }
+
+    /**
+     * [DateTimeParseException]のハンドリングを行う
+     */
+    @Suppress("UNUSED")
+    @ExceptionHandler(DateTimeParseException::class)
+    fun handleDateTimeParseException(ex: DateTimeParseException): ResponseEntity<Void> {
         logger.info("Input validation failed.: {}", ex.message)
         return ResponseEntity.badRequest().build()
     }
