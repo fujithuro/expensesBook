a2291e2ad58dd82ff4a2446c2f7b7f211a5186b1
fujita <fujithuro@gmail.com>
2024-09-08 19:28:54 +0900

    feat: 独自例外に原因を設定できるようにする

--- src/main/kotlin/com/thurofuji/expensesBook/exception/ExpenseBookException.kt
+++ src/main/kotlin/com/thurofuji/expensesBook/exception/ExpenseBookException.kt
@@ -3,9 +3,9 @@ package com.thurofuji.expensesBook.exception
 /**
  * 家計簿アプリケーションで発生した問題を扱うための独自例外
  */
-sealed class ExpenseBookException(message: String): RuntimeException(message)
+sealed class ExpenseBookException(message: String, cause: Throwable? = null): RuntimeException(message)
 
 /** 費目コードが不正な値だった場合の例外 */
-class InvalidExpenseTypeException(message: String): ExpenseBookException(message)
+class InvalidExpenseTypeException(message: String, cause: Throwable? = null): ExpenseBookException(message, cause)
 /** 対象年月が不正な値だった場合の例外 */
-class InvalidTargetYearMonthException(message: String): ExpenseBookException(message)
+class InvalidTargetYearMonthException(message: String, cause: Throwable? = null): ExpenseBookException(message, cause)
