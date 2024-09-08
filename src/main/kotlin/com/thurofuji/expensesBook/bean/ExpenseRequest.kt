dda29b76fc4e6f87f09a219e06b86a010b41396f
fujita <fujithuro@gmail.com>
2024-09-08 15:17:29 +0900

    feat: リクエストボディで`id`を送ることはないのでプロパティから削除

--- src/main/kotlin/com/thurofuji/expensesBook/bean/ExpenseRequest.kt
+++ src/main/kotlin/com/thurofuji/expensesBook/bean/ExpenseRequest.kt
@@ -3,13 +3,11 @@ package com.thurofuji.expensesBook.bean
 import com.thurofuji.expensesBook.annotation.ValidExpenseType
 import jakarta.validation.constraints.NotNull
 import java.time.LocalDate
-import java.util.UUID
 
 /**
  * リクエストされた出費を表すデータクラス
  */
 data class ExpenseRequest(
-    val id: UUID? = null,
     @field:NotNull
     val date: LocalDate?,
     @field:NotNull
