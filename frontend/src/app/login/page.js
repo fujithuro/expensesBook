871aebdc51c0295d1e28610b1adfc6f72280c360
fujita <fujithuro@gmail.com>
2024-09-28 00:22:10 +0900

    refactor: クラス数が多すぎて分かりにくかったので、テンプレートリテラルを使って改行する。まだ改善できる気はするが、画面数やコンポーネント数が増えた場合に追って考えることにする。
    refs #2 フロントエンドの作成

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -15,7 +15,20 @@ export default function LoginPage() {
                             </label>
                             <div className="mt-1">
                                 <input type="text" id="username" name="username" required
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    className="{`
+                                        appearance-none
+                                        block
+                                        w-full
+                                        px-3 py-2
+                                        border border-gray-300
+                                        rounded-md
+                                        shadow-sm
+                                        placeholder-gray-400
+                                        focus:outline-none
+                                        focus:ring-indigo-500
+                                        focus:border-indigo-500
+                                        sm:text-sm
+                                    `}"
                                 />
                             </div>
                         </div>
@@ -25,11 +38,42 @@ export default function LoginPage() {
                             </label>
                             <div className="mt-1">
                                 <input type="password" id="password" name="password" required
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"/>
+                                    className="{`
+                                        appearance-none
+                                        block
+                                        w-full
+                                        px-3 py-2
+                                        border border-gray-300
+                                        rounded-md
+                                        shadow-sm
+                                        placeholder-gray-400
+                                        focus:outline-none
+                                        focus:ring-indigo-500
+                                        focus:border-indigo-500
+                                        sm:text-sm
+                                    `}"/>
                             </div>
                         </div>
                         <div>
-                            <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <button type="submit"
+                                    className="{`
+                                        w-full
+                                        flex
+                                        justify-center
+                                        py-2 px-4
+                                        border border-transparent
+                                        rounded-md
+                                        shadow-sm
+                                        text-sm
+                                        font-medium
+                                        text-white
+                                        bg-indigo-600
+                                        hover:bg-indigo-700
+                                        focus:outline-none
+                                        focus:ring-2
+                                        focus:ring-offset-2
+                                        focus:ring-indigo-500
+                                    `}">
                                 Login
                             </button>
                         </div>
