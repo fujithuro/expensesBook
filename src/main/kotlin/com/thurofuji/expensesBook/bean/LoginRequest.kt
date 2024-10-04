09db339d90570e932ff738fdb591c5cbf504260a
fujita <fujithuro@gmail.com>
2024-10-04 22:31:30 +0900

    feat: サーバーサイドのログイン処理を実装する。内容は非常に暫定的なものなので、TODOコメントを残している
    refs #2 フロントエンドの作成

---
+++ src/main/kotlin/com/thurofuji/expensesBook/bean/LoginRequest.kt
@@ -0,0 +1,3 @@
+package com.thurofuji.expensesBook.bean
+
+data class LoginRequest(val username: String, val password: String)
---
+++ src/main/kotlin/com/thurofuji/expensesBook/controller/LoginController.kt
@@ -0,0 +1,26 @@
+package com.thurofuji.expensesBook.controller
+
+import com.thurofuji.expensesBook.bean.LoginRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class LoginController {
+
+    // TODO 現在の構成だとフロントエンド（`localhost:3000`）とサーバーサイド（`localhost:8080`）が
+    //  CrossOriginになってしまうのでアノテーションを付与している。将来的には構成を見直して不要とする予定
+    @CrossOrigin(origins = ["http://localhost:3000"])
+    @PostMapping("/api/login")
+    fun login(@RequestBody request: LoginRequest): ResponseEntity<Void> {
+        // TODO ひとまず`ok`と`Bad Request`を分けるための最低限の実装のみ書いているので、正式なログイン処理に改善する
+        return if (request.username == "admin") {
+            ResponseEntity.ok().build()
+        } else {
+            ResponseEntity.badRequest().build()
+        }
+    }
+
+}
