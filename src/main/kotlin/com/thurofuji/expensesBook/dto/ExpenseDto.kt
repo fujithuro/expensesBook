3de2b1775e83c355bfc81f36167a83ac5441d5db
fujita <fujithuro@gmail.com>
2024-12-17 23:04:24 +0900

    style: 見やすくするため、カンマ位置を行の最後にする。末尾カンマも追加することで、余計な差分が出る問題にも対応できている

--- src/main/kotlin/com/thurofuji/expensesBook/dto/ExpenseDto.kt
+++ src/main/kotlin/com/thurofuji/expensesBook/dto/ExpenseDto.kt
@@ -6,13 +6,15 @@ import java.util.UUID
 /**
  * 主にServiceの層で、DBに登録済みの出費1件を扱うためのモデル
  */
-data class ExpenseDto(val id: UUID
-                      , val 支払日: LocalDate
-                      , val 費目: ExpenseTypeDto
-                      , val 金額: Int
-                      , val 支払先: String
-                      , val 使途: String
-                      , val 最終更新者id: Int) {
+data class ExpenseDto(
+    val id: UUID,
+    val 支払日: LocalDate,
+    val 費目: ExpenseTypeDto,
+    val 金額: Int,
+    val 支払先: String,
+    val 使途: String,
+    val 最終更新者id: Int,
+) {
     constructor(id: UUID, newDto: NewExpenseDto): this(
         id, newDto.支払日, newDto.費目, newDto.金額, newDto.支払先, newDto.使途, newDto.登録者id
     )
